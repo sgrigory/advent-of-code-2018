5e820662ab93f01e4e686758506f4374dd07a89c
sgrigory <grisha.sizov@gmail.com>
2021-05-19 23:50:45 +0200

    add day 1

---
+++ day1_2018.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+
+	let content = fs::read_to_string("day1_input.txt").expect("error");
+	//println!("{}", content);
+
+	let res: Vec<i32> = content.trim().split("\n").map(|x| x.parse::<i32>().unwrap()).collect();
+	let res_sum: i32 = res.iter().sum();
+	println!("{}", res_sum);
+
+	let mut curr = 0;
+	let mut i = 0;
+	let mut set = HashSet::new();
+	let mut found = false;
+	while !found {
+
+		curr += res[i];
+		i = (i + 1) % res.len();
+		if set.contains(&curr) {
+			println!("{}", curr);
+			found = true;
+		}
+		set.insert(curr);
+	}
+
+
+}
