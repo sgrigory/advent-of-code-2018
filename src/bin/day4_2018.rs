06c03e0f45c266e21bb7f3719b3d73fe294b37a8
sgrigory <grisha.sizov@gmail.com>
2021-06-10 11:22:14 +0200

    add solution to day 4 part 2

--- src/bin/day4_2018.rs
+++ src/bin/day4_2018.rs
@@ -17,7 +17,6 @@ fn get_entry(s: &str) -> Entry {
 	// input looks like: [1518-11-01 00:00] Guard #10 begins shift
 	let entry_re = Regex::new(r"\[(.+)\] (.+)$").unwrap();
 
-	println!("{}", s);	
 	let parsed = entry_re.captures(s).unwrap();
 	let time_unparsed: &str = parsed.get(1).map_or("", |m| m.as_str());//.parse().unwrap(); // 1518-04-30 00:00
 	let time = NaiveDateTime::parse_from_str(&time_unparsed, "%Y-%m-%d %H:%M").unwrap();
@@ -31,7 +30,6 @@ fn get_entry(s: &str) -> Entry {
 fn write_interval(timetable: &mut HashMap<i32, [i32;60]>, fall_asleep: NaiveDateTime, wake_up: NaiveDateTime, guard: i32) -> () {
 	let minutes_start = fall_asleep.time().minute() as usize;
 	let minutes_end = wake_up.time().minute() as usize;
-	println!("{}", guard);
 	for i in minutes_start..minutes_end {
 		if timetable.contains_key(&guard) {
 			(*timetable.get_mut(&guard).unwrap())[i] += 1
@@ -46,20 +44,25 @@ fn main() {
 
 	let file_content = fs::read_to_string("inputs/day4_input.txt").expect("error");
 	let mut entries = file_content.trim().split("\n").map(get_entry).collect::<Vec<Entry>>();
+	// Sort entries chronologically
 	entries.sort_by(|a, b | a.time.cmp(&b.time));
 	
+	// Current guard
 	let mut last_guard = 0;
+	// Time of the last registered "fall asleep" event
 	let mut last_fall_asleep = NaiveDateTime::parse_from_str("2021-01-01 21:00", "%Y-%m-%d %H:%M").unwrap();
+	// Regex to parse new guard entries
 	let new_guard_re = Regex::new(r"Guard #(\d+) begins shift$").unwrap();
 
+	// Keys are guards, indices of the array are minutes, values of the array are amounts of sleep
 	let mut timetable: HashMap<i32, [i32;60]> = HashMap::new();
 
+	// Go over all entries, fill in the timetable
 	for entry in entries {
-		println!("new entry: {}", entry.text);
 		match entry.text.as_str() {
 			"wakes up" => write_interval(&mut timetable, last_fall_asleep, entry.time, last_guard),
 			"falls asleep" => last_fall_asleep = entry.time,
-			_ => last_guard = { println!("last guard entry: {}", entry.text);
+			_ => last_guard = { 
 				     			new_guard_re.captures(&entry.text).unwrap().get(1).map_or("", |m| m.as_str()).parse().unwrap()
 							},
 		}
@@ -67,6 +70,7 @@ fn main() {
 
 	let sums = timetable.iter().map(|x| (*x.0, x.1.iter().sum::<i32>()));
 
+	// Find guard with max total sleep
 	let mut max_sleep = 0;
 	let mut chosen_guard = 0;
 	for (k, v) in sums {
@@ -77,10 +81,18 @@ fn main() {
 	}
 	println!("chosen_guard = {}", chosen_guard);
 
+	// Find minute on which the chosen guard had the most sleep
 	let minute_max_sleep = timetable[&chosen_guard].iter().enumerate().max_by_key(|x| x.1).unwrap().0;
 
 	println!("minute_max_sleep = {}", minute_max_sleep);
 
-	println!("answer to part 1 = {}", minute_max_sleep as i32 * chosen_guard)
+	println!("answer to part 1 = {}", minute_max_sleep as i32 * chosen_guard);
+
+	// Find minute with max total sleep per guard
+	let max_by_guard = timetable.iter().map(|x| (x.0, x.1.iter().enumerate().max_by_key(|x| x.1).unwrap()));
+	// Find the guard and the minute with max total sleep overall
+	let max_overall = max_by_guard.max_by_key(|x| x.1.1).unwrap();
+
+	println!("answer to part 2 = {}", max_overall.0 * max_overall.1.0 as i32);
 
 }
